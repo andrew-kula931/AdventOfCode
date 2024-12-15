b75cef6a2d018cd4385c70439c5ea394afe5af5e
Andrew Kula <arkula931@gmail.com>
2024-12-15 02:18:20 -0600

    start of day15

--- fileInput.js
+++ fileInput.js
@@ -2,12 +2,10 @@ var fs = require("fs");
 
 var filename = process.argv[2];
 if (!filename) {
-    console.error("Please provide a filename");
-    process.exit(1);
+  console.error("Please provide a filename");
+  process.exit(1);
 }
 var input = fs.readFileSync(filename, "utf-8");
 var lines = input.split("\r\n").filter(Boolean);
 
-lines.forEach(function (line) {
-   
-});
+lines.forEach(function (line) {});
